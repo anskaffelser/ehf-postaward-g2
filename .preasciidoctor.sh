9a1e295dde7b48eec636d72b5d681f7332889ee6
difi <erlend@klakegg.net>
2016-08-18 23:05:38 +0200

    Minor change in .preasciidoctor.sh.

--- .preasciidoctor.sh
+++ .preasciidoctor.sh
@@ -6,7 +6,7 @@ for x in $(find rules/*/xsl -type f); do
 
 	echo "Generate table for $(basename $x)"
 
-	saxon-xslt -s:$x -xsl:tools/svrlxslt2schematron.xsl -o:/tmp/rules/$(basename $x).sch
-	saxon-xquery -s:/tmp/rules/$(basename $x).sch -q:tools/rules_asciidoc.xquery -o:/tmp/rules/$(basename $x).adoc
+	saxon-xslt -s:$x -xsl:tools/svrlxslt2schematron.xsl -o:$x.sch
+	saxon-xquery -s:$x.sch -q:tools/rules_asciidoc.xquery -o:/tmp/rules/$(basename $x).adoc
 
 done
