aa2c57def891cab5d522e619f937924be7c868be
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-10-02 11:31:28 +0200

    Adding some tests for Reminder.

--- .preasciidoctor.sh
+++ .preasciidoctor.sh
@@ -2,14 +2,14 @@
 
 mkdir -p /tmp/rules
 
-for x in $(find rules/*/xsl -type f); do
+#for x in $(find rules/*/xsl -type f); do
 
-	echo "Generate table for $(basename $x)"
+#	echo "Generate table for $(basename $x)"
 
-	saxon-xslt -s:$x -xsl:tools/svrlxslt2schematron.xsl -o:$x.sch
-	saxon-xquery -s:$x.sch -q:tools/rules_asciidoc.xquery -o:/tmp/rules/$(basename $x).adoc
+#	saxon-xslt -s:$x -xsl:tools/svrlxslt2schematron.xsl -o:$x.sch
+#	saxon-xquery -s:$x.sch -q:tools/rules_asciidoc.xquery -o:/tmp/rules/$(basename $x).adoc
 
-done
+#done
 
 for x in $(find rules/*/sch -type f); do
 
