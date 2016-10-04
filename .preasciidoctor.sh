540e04685ef1a1d8abf19a32bb236cd26a71f0a1
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-10-04 16:05:47 +0200

    Cleaning up old functionality.

--- .preasciidoctor.sh
+++ .preasciidoctor.sh
@@ -2,15 +2,6 @@
 
 mkdir -p /tmp/rules
 
-#for x in $(find rules/*/xsl -type f); do
-
-#	echo "Generate table for $(basename $x)"
-
-#	saxon-xslt -s:$x -xsl:tools/svrlxslt2schematron.xsl -o:$x.sch
-#	saxon-xquery -s:$x.sch -q:tools/rules_asciidoc.xquery -o:/tmp/rules/$(basename $x).adoc
-
-#done
-
 for x in $(find rules/*/sch -type f); do
 
 	echo "Generate table for $(basename $x)"
